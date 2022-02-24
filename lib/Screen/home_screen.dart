ffd6481fde107f6c2590b2786a7aa471ca7a78e4
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2022-02-25 00:24:11 +0600

    Update home screen

--- lib/Screen/home_screen.dart
+++ lib/Screen/home_screen.dart
@@ -15,66 +15,89 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   User? user = FirebaseAuth.instance.currentUser;
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
   UsersModel loggedInUser = UsersModel();
 
-  @override
-  void initState() {
-    super.initState();
-    FirebaseFirestore.instance
-        .collection('users')
-        .doc(user!.uid)
-        .get()
-        .then((value) {
-      loggedInUser = UsersModel.fromMap(value.data());
-    });
-    setState(() {});
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   FirebaseFirestore.instance
+  //       .collection('users')
+  //       .doc(user!.uid)
+  //       .get()
+  //       .then((value) {
+  //     loggedInUser = UsersModel.fromMap(value.data());
+  //   });
+  //   setState(() {});
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Center(child: Text('Welcome')),
-        backgroundColor: Colors.redAccent,
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(35.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            // ignore: prefer_const_literals_to_create_immutables
-            children: [
-              // ignore: prefer_const_constructors
-              Text(
-                'Welcome Back',
-                // ignore: prefer_const_constructors
-                style: TextStyle(
-                  fontSize: 35,
-                  color: Colors.black87,
-                  fontWeight: FontWeight.bold,
+    return FutureBuilder<DocumentSnapshot>(
+      future: users.doc(user!.uid).get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text("Something went wrong");
+        }
+
+        if (snapshot.hasData && !snapshot.data!.exists) {
+          return Text("Document does not exist");
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data =
+              snapshot.data!.data() as Map<String, dynamic>;
+          return Scaffold(
+            backgroundColor: Colors.white,
+            appBar: AppBar(
+              title: Center(child: Text('Welcome')),
+              backgroundColor: Colors.redAccent,
+            ),
+            body: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(35.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    // ignore: prefer_const_constructors
+                    Text(
+                      'Welcome Back',
+                      // ignore: prefer_const_constructors
+                      style: TextStyle(
+                        fontSize: 35,
+                        color: Colors.black87,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text('${data['firstName']} ${data['lastName']}'),
+                    SizedBox(height: 20),
+                    Text(
+                      '${data['email']}',
+                      // loggedInUser['firstName'].toString(),
+                    ),
+                    SizedBox(height: 20),
+                    ActionChip(
+                      label: Text('Logout'),
+                      backgroundColor: Colors.redAccent,
+                      onPressed: () {
+                        logout(context);
+                      },
+                    ),
+                  ],
                 ),
               ),
-              SizedBox(height: 20),
-              Text('${loggedInUser.firstName} ${loggedInUser.lastName}'),
-              SizedBox(height: 20),
-              Text(
-                '${loggedInUser.email}',
-                // loggedInUser['firstName'].toString(),
-              ),
-              SizedBox(height: 20),
-              ActionChip(
-                label: Text('Logout'),
-                backgroundColor: Colors.redAccent,
-                onPressed: () {
-                  logout(context);
-                },
-              ),
-            ],
-          ),
-        ),
-      ),
+            ),
+          );
+        }
+
+        return Scaffold(
+            body: Center(
+                child: Text("loading......", style: TextStyle(fontSize: 25))));
+      },
     );
   }
 
